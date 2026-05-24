9ffcb700916bd6b962d169ea7f0004a74ec7e741
Alexandr <slicarenko@gmail.com>
2026-05-24 17:17:09 +0300

    Second step

---
+++ ShopPersist/src/test/java/ru/slisarenko/shoppersist/ShopPersistApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.slisarenko.shoppersist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPersistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
